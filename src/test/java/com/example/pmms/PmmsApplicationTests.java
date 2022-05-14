c34595b6e6b321c2c56482e7b4181beddc389b9b
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2022-05-14 15:56:48 +0530

    initial commit

---
+++ src/test/java/com/example/pmms/PmmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
